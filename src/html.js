3368d87eb65e0d8bcbaff507525e367b61b69a44
unknown <waltinhocalegari@hotmail.com>
2021-10-19 15:41:56 -0300

    cala a boca andreza

--- src/html.js
+++ src/html.js
@@ -11,6 +11,11 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6982161637686870"
+          crossorigin="anonymous"
+        ></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="dark">
