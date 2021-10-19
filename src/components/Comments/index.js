7c71f5d8dee2d385e3498733eb8990474144d409
unknown <waltinhocalegari@hotmail.com>
2021-10-19 18:46:59 -0300

    mas tá

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,13 +5,13 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `https://stoic-meitner-039dc1.netlify.app/${url}`
+  const completeURL = `https://stoic-meitner-039dc1.netlify.app/`
 
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
       <ReactDisqusComments
-        shortname="blog"
+        shortname="disqus_A9ozMrHbyH"
         identifier={completeURL}
         title={title}
         url={completeURL}
